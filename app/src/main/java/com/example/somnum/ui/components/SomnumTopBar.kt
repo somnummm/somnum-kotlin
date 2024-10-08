046b337eaacd78d6d4a2969ab69714d09149aeaf
Liès <lies.tiguercha@outlook.com>
2024-10-08 15:27:35 +0200

    :sparkles: Adding a top bar with a account button

--- app/src/main/java/com/example/somnum/ui/components/SomnumTopBar.kt
+++ app/src/main/java/com/example/somnum/ui/components/SomnumTopBar.kt
@@ -1,2 +1,55 @@
 package com.example.somnum.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SomnumTopBar(titleText: String, onProfileClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp, 32.dp, 16.dp, 16.dp)
+            .background(Color.Transparent),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = titleText,
+            color = MaterialTheme.colorScheme.primary,
+            fontSize = 26.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.weight(1f)
+        )
+        IconButton(onClick = onProfileClick) {
+            Box(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.primary,
+                        shape = MaterialTheme.shapes.small
+                    )
+                    .padding(8.dp)
+            ) {
+                Icon(
+                    Icons.Filled.Person,
+                    contentDescription = "Profil",
+                    tint = Color.Black
+                )
+            }
+        }
+    }
+}
